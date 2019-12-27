ba779e70c169d7e871c25c48d51026d45afabaf8
Juan Di Nella <juan.dinella@gmail.com>
2019-12-27 11:26:21 -0300

    remove posthtml/cssmodules

--- src/pipes/html.js
+++ src/pipes/html.js
@@ -1,21 +1,15 @@
 import lazypipe from 'lazypipe'
-import modules from 'posthtml-css-modules'
-import postHtml from 'gulp-posthtml'
 import prettify from 'gulp-jsbeautifier'
 import replace from 'gulp-html-replace'
 
 const html = ({
-  cssModules,
   htmlReplace,
   plugins = []
 }) => {
-  const defaultPlugins = [
-    modules(cssModules),
-  ]
-  const postHtmlPlugins = [...defaultPlugins, ...plugins]
+  const postHtmlPlugins = [...plugins]
   return lazypipe()
     .pipe(replace, htmlReplace)
-    .pipe(postHtml, postHtmlPlugins)
+    .pipe(postHtmlPlugins)
     .pipe(prettify)
 }
 
--- src/pipes/pug.js
+++ src/pipes/pug.js
@@ -1,22 +1,16 @@
 import lazypipe from 'lazypipe'
 import compiler from 'gulp-pug'
-import modules from 'posthtml-css-modules'
-import postHtml from 'gulp-posthtml'
 import prettify from 'gulp-jsbeautifier'
 
 const pug = ({
-  cssModules,
   pug,
   plugins = []
 }) => {
-  const defaultPlugins = [
-    modules(cssModules),
-  ]
 
-  const postHtmlPlugins = [...defaultPlugins, ...plugins]
+  const postHtmlPlugins = [...plugins]
   return lazypipe()
     .pipe(compiler, pug)
-    .pipe(postHtml, postHtmlPlugins)
+    .pipe(postHtmlPlugins)
     .pipe(prettify)
 }
 
